ce906080d992f4fb9276c8604029545d44873243
Mitchell Skaggs <skaggsm333@gmail.com>
2015-11-10 06:22:49 -0600

    Initial commit

---
+++ src/main/java/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+/**
+ * Created by Mitchell on 11/10/2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
