a362e3a93dad6e43d1218abc2f8f4f70dea1636c
Mitchell Skaggs <skaggsm333@gmail.com>
2015-11-10 06:37:28 -0600

    Reformat

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -4,8 +4,7 @@ package helloworld;
  * Created by Mitchell on 11/10/2015.
  */
 public class HelloWorld {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
